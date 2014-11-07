d18080b4bf1f4b4bbb379af2a9873be378dccbed
Michael Wyraz <michael.wyraz@evermind.de>
2014-11-07 09:01:10 +0100

    test

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 if [ $UID != 0 ]; then
-  echo "Exec $0 as root"
+  echo "I'm not root. running sudo $0"
   sudo $0
   exit $?
 fi
 
 echo "Now running as root..."
+
+if [ ! -f /usr/bin/debootstrap ]; then
+  echo "Installing debootstrap"
+  apt-get install debootstrap
+fi
 
